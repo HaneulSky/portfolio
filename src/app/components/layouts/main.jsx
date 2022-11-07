dc92e5688efdc9c03949361cb7fd587fa081d2e1
HaneulSky <tanya0610@naver.com>
2022-11-07 13:29:13 +0300

    added title

--- src/app/components/layouts/main.jsx
+++ src/app/components/layouts/main.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import About from "./about/about";
 import Contacts from "../ui/contacts/contacts";
 
 const Main = () => {
+    useEffect(() => {
+        document.title = "Обо мне";
+    }, []);
+
     return (
         <div className="pt-4">
             <About />
