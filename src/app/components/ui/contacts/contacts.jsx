bc63b35732b0125117c3a3c0347b272d5fa019e2
HaneulSky <tanya0610@naver.com>
2022-11-26 15:59:49 +0300

    исправила переход по ссылкам и резюме

--- src/app/components/ui/contacts/contacts.jsx
+++ src/app/components/ui/contacts/contacts.jsx
@@ -4,40 +4,59 @@ import contactsStyle from "./contacts.module.css";
 
 const Contacts = () => {
     return (
-            <ul className={contactsStyle.contacts}>
-                <li className={contactsStyle.contact}>
-                    <a href="https://github.com/HaneulSky">
-                        <i className="bi bi-github" style={{ fontSize: 1.5 + "rem" }}></i>
-                    </a>
-                </li>
-                <li className={contactsStyle.contact}>
-                    <a href="https://t.me/kotatiana">
-                        <i className="bi bi-telegram" style={{ fontSize: 1.5 + "rem" }}></i>
-                    </a>
-                </li>
-                <li className={contactsStyle.contact}>
-                    <i className="bi bi-envelope"></i>{" "}
-                    <a
-                        href="mailto:ko20tatiana@yandex.ru"
-                        role="button"
-                        className="btn btn-light "
-                    >
-                        ko20tatiana@yandex.ru
-                    </a>
-                </li>
-                <li className={contactsStyle.contact}>
-                    <i className="bi bi-telephone"></i> <i className="bi bi-whatsapp"></i>  8(921)388-40-24
-                </li>
-                <li className={contactsStyle.contact}>
-                    <a
-                        href="https://drive.google.com/file/d/1vKcGXVWSyVItrNdRmxe33-dJ2DtxaKYN/view?usp=sharing"
-                        role="button"
-                        className="btn btn-light "
-                    >
-                        <i className="bi bi-cloud-arrow-down"></i> Мое резюме
-                    </a>
-                </li>
-            </ul>
+        <ul className={contactsStyle.contacts}>
+            <li className={contactsStyle.contact}>
+                <a
+                    href="https://github.com/HaneulSky"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <i
+                        className="bi bi-github"
+                        style={{ fontSize: 1.5 + "rem" }}
+                    ></i>
+                </a>
+            </li>
+            <li className={contactsStyle.contact}>
+                <a
+                    href="https://t.me/kotatiana"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <i
+                        className="bi bi-telegram"
+                        style={{ fontSize: 1.5 + "rem" }}
+                    ></i>
+                </a>
+            </li>
+            <li className={contactsStyle.contact}>
+                <i className="bi bi-envelope"></i>{" "}
+                <a
+                    href="mailto:ko20tatiana@yandex.ru"
+                    role="button"
+                    className="btn btn-light "
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    ko20tatiana@yandex.ru
+                </a>
+            </li>
+            <li className={contactsStyle.contact}>
+                <i className="bi bi-telephone"></i>{" "}
+                <i className="bi bi-whatsapp"></i> 8(921)388-40-24
+            </li>
+            <li className={contactsStyle.contact}>
+                <a
+                    href="https://drive.google.com/file/d/1588KWpM7MQFqaYdw0zUT7Vg30Axkq9Jy/view?usp=sharing"
+                    role="button"
+                    className="btn btn-light "
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <i className="bi bi-cloud-arrow-down"></i> Мое резюме
+                </a>
+            </li>
+        </ul>
     );
 };
 
--- src/app/components/ui/projectCard/projectCard.jsx
+++ src/app/components/ui/projectCard/projectCard.jsx
@@ -7,15 +7,36 @@ const ProjectCard = ({ image, alt, children, link, page }) => {
     const cardClasses = `card shadow ${projectCardStyles.card}`;
     return (
         <div className={cardClasses}>
-            <img src={image} className="card-img-top" alt={alt} style={{ maxWidth: 100 + "%", height: 150 + "px" }} />
+            <img
+                src={image}
+                className="card-img-top"
+                alt={alt}
+                style={{ maxWidth: 100 + "%", height: 150 + "px" }}
+            />
             <div className="card-body d-flex flex-column">
-                <div className= {projectCardStyles.text}>{children}</div>
+                <div className={projectCardStyles.text}>{children}</div>
                 <div className="align-self-end mt-auto">
-                    <a href={page} title="Посмотреть сайт">
-                        <i className="bi bi-box-arrow-up-left" style={{ fontSize: 30 + "px" }}></i>
+                    <a
+                        href={page}
+                        title="Посмотреть сайт"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i
+                            className="bi bi-box-arrow-up-left"
+                            style={{ fontSize: 30 + "px" }}
+                        ></i>
                     </a>{" "}
-                    <a href={link} title="Посмотреть код проекта">
-                        <i className="bi bi-github" style={{ fontSize: 30 + "px" }}></i>
+                    <a
+                        href={link}
+                        title="Посмотреть код проекта"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <i
+                            className="bi bi-github"
+                            style={{ fontSize: 30 + "px" }}
+                        ></i>
                     </a>
                 </div>
             </div>
